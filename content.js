6d66b3cc875691affe34009c1b78433875bab734
psw01 <sahanpasindu326@gmail.com>
2021-07-26 21:53:41 +0530

    added focus mode

--- content.js
+++ content.js
@@ -1,13 +1,12 @@
 var Link = window.location.toString()
 var Hook = ''
 
-
 if (!localStorage.getItem('PP_hook'))
     ask_hook();
 Hook = localStorage.getItem('PP_hook');
 
 if (Link != '') {
-    var sending_prompt = confirm("send to discord ?");
+    var sending_prompt = false //confirm("send to discord ?");
     if (!localStorage.getItem('PP_username'))
         localStorage.setItem('PP_username', prompt("Username: "));
     var username = localStorage.getItem('PP_username');
@@ -36,15 +35,9 @@ document.addEventListener('keydown', (event) => {
         if (input == '/resize')
             resize()
     }
-    if (event.code == 'Escape') {
-        if (elm = document.body.children[0].hidden) {
-            elm = document.body.children[0].hidden = false
-            document.getElementsByClassName('left_wrapper')[0].style.width = '60%'
-        } else {
-            elm = document.body.children[0].hidden = true
-            document.getElementsByClassName('left_wrapper')[0].style.width = '100%'
-        }
-    }
+    if (event.code == 'Escape')
+        scaler(!document.body.children[0].hidden)
+
 }, false);
 
 
@@ -57,11 +50,14 @@ function resize() {
     document.getElementsByClassName('left_wrapper')[0].style.width = `${pp}%`
 }
 
-
-
-
-
-
+function scaler(toggle) {
+    elm = document.body.children
+    elm[0].hidden = elm[2].hidden = elm[3].hidden = toggle
+    document.querySelector('main').style.padding = 0
+    document.querySelector('.info').style.position = 'absolute'
+    document.querySelectorAll("body > *:not(.left_wrapper) > *").forEach(e => { e.hidden = toggle });
+    document.getElementsByClassName('left_wrapper')[0].style.width = (toggle) ? "99%" : "60%"
+}
 
 
 
